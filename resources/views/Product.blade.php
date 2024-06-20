e88cb3a7d7967dcee95eaa2068ee40ba5af70944
Emerson Bagawe <eob2286@students.uc-bcf.edu.ph>
2024-06-20 19:01:20 +0800

    created blade file and displayed the products

--- resources/views/Product.blade.php
+++ resources/views/Product.blade.php
@@ -1,3 +1,20 @@
-<div>
-    <!-- Order your soul. Reduce your wants. - Augustine -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body style="background-color: 	#3e3e42" >
+    <div style="color:azure; Black; Text-align:Center;">
+        <h1>Movies</h1>
+        <p><strong>HORROR:</strong> {{$movies['horror']}}</p>
+        <p><strong>ACTION:</strong> {{$movies['action']}}</p>
+        <p><strong>ADVENTURE:</strong> {{$movies['adventure']}}</p>
+        <p><strong>ROMANCE:</strong> {{$movies['romance']}}</p>
+        <p><strong>COMEDY:</strong> {{$movies['comedy']}}</p>
+    </div> 
+</body>
+</html>
+
